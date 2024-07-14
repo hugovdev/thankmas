c6cebb84937c836d33084e66e2502c575e8ada4e
Hugo Vallejo <hugo.vallejo@noxcrew.com>
2024-07-14 18:05:31 +0200

    Remove unnecessary import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
-
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.papermc.paperweight.userdev") version "1.7.1" apply false
