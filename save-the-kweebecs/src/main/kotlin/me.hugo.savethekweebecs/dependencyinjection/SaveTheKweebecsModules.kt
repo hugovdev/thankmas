c872f51bb25fa82166134a1768efda0b33e1ed70
Hugo Vallejo <hugov2002@gmail.com>
2024-11-17 16:32:09 +0100

    Trying to make it a submodule!

--- save-the-kweebecs/src/main/kotlin/me.hugo.savethekweebecs/dependencyinjection/SaveTheKweebecsModules.kt
+++
@@ -1,8 +0,0 @@
-package me.hugo.savethekweebecs.dependencyinjection
-
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
-
-@Module
-@ComponentScan("me.hugo.savethekweebecs")
-public class SaveTheKweebecsModules
